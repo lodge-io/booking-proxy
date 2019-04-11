ec9f595b2758ef3dc7f5cbccdca381f275953fec
carl12 <carl17r@gmail.com>
2019-04-11 16:26:33 -0700

    Style booking-module to not cover images

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const port = 8001;
 
+const bookingPath = 'http://localhost:3000/listings/';
 
 app.use(express.static('dist'));
 
-app.listen(8001, () => {
-  console.log('listening');
+app.get('/listings/:listingId', (req, res) => {
+  console.log(req.params.listingId);
+  axios.get(bookingPath + req.params.listingId)
+    .then(data => {
+      res.send(data.data);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
 });
